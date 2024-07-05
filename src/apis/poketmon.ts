fe9f154c6ca5687807ac4e50aa22db45702deca3
parkchowon <70216263+parkchowon@users.noreply.github.com>
2024-07-05 12:29:38 +0900

    Fix: url주소 수정

--- src/apis/poketmon.ts
+++ src/apis/poketmon.ts
@@ -1,13 +1,15 @@
 import axios from "axios";
 
 export const fetchPokemon = async (id: string) => {
-  const response = await fetch(`http://localhost:3000/api/pokemons/${id}`);
+  const response = await fetch(
+    `https://pokemon-dogam.vercel.app/api/pokemons/${id}`
+  );
   return response.json();
 };
 
 export const fetchPokemons = async (id: string) => {
   const response = await axios.get(
-    `http://localhost:3000/api/pokemons?page=${id}`
+    `https://pokemon-dogam.vercel.app/api/pokemons?page=${id}`
   );
   const data = await response.data.data;
   return data;
